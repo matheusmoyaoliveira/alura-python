e536a3e084e25d5612f95ad202a97c241f74dd0d
Matheus Moya de Oliveira <167132031+matheusmoyaoliveira@users.noreply.github.com>
2024-10-18 04:07:49 -0300

    sabor-express

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -1,6 +1,6 @@
 import os
 
-restaurantes = []
+restaurantes = ['Pizza', 'Sushi']
 
 def exibir_nome_do_programa():
     print('Sabor Express\n')
@@ -12,23 +12,37 @@ def exibir_opçoes():
     print('4. Sair\n')
 
 def finalizar_app():
-    os.system('cls')
+    exibir_subtitulo('Finalizando o app')
     # os.system('clear') no mac
-    print('Finalizando o app\n')
+    
+
+def voltar_ao_menu_principal():
+    input('\nDigite uma tecla para voltar ao menu ')
+    main()
 
 def opçao_invalida():
     print('Opção inválida!\n')
-    input('Digite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
+
+def exibir_subtitulo(texto):
+    os.system('cls')
+    print(texto)
+    print()
 
 def cadastrar_novo_restaurante():
-    os.system('cls')
-    print('Cadastro de novos restaurantes\n')
+    exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     restaurantes.append(nome_do_restaurante)
     print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!')
-    input('Digite uma tecla para voltar ao menu principal')
-    main()
+    voltar_ao_menu_principal()
+
+def listar_restaurantes():
+    exibir_subtitulo('Listando os restaurantes')
+
+    for restaurante in restaurantes:
+        print(f'.{restaurante}')
+
+    voltar_ao_menu_principal()
 
 def escolher_opçao():
     try:
@@ -36,9 +50,9 @@ def escolher_opçao():
         # opçao_escolhida = int(opçao_escolhida)
             
         if opçao_escolhida == 1:
-            print('Cadastrar restaurante')
+            cadastrar_novo_restaurante()
         elif opçao_escolhida == 2:
-            print('Listar restaurantes')
+            listar_restaurantes()
         elif opçao_escolhida == 3:
             print('Ativar restaurante')
         elif opçao_escolhida == 4:
