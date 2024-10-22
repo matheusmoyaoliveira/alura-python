c2101d53d90f5a6a9db65698bc11ccdd99722140
Matheus Moya de Oliveira <167132031+matheusmoyaoliveira@users.noreply.github.com>
2024-10-22 18:46:31 -0300

    Finalizando com as Docstrings

--- sabor-express/app.py
+++ sabor-express/app.py
@@ -34,6 +34,16 @@ def exibir_subtitulo(texto):
     print()
 
 def cadastrar_novo_restaurante():
+    ''' Essa funcção é responsável por cadastrar um novo restaurante
+    
+    Inputs:
+    - Nome do restaurante
+    - Categoria
+
+    Outputs:
+    - Adiciona um novo restaurante a lista de restaurantes
+
+    '''
     exibir_subtitulo('Cadastro de novos restaurantes')
     nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
     categoria = input(f'Digite o nome da categoria do restaurante {nome_do_restaurante}: ')
