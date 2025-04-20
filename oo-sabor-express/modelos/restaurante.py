b0fb0cb19c89b1f7c5076896ea09ac2d49470a8c
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-04-20 20:41:31 -0300

    Praticando e continuando projeto

--- oo-sabor-express/modelos/restaurante.py
+++ oo-sabor-express/modelos/restaurante.py
@@ -1,14 +1,30 @@
 class Restaurante:
-    nome = ''
-    categoria = ''
-    ativo = False
+    restaurantes = []
 
-restaurante_praca = Restaurante()
-restaurante_praca.nome = 'Praça'
-restaurante_praca.categoria = 'Gourmet'
+    def __init__(self, nome, categoria):
+        self._nome = nome.title()
+        self.categoria = categoria.upper()
+        self._ativo = False
+        Restaurante.restaurantes.append(self)
 
-restaurante_pizza = Restaurante()
+    def __str__(self):
+        return f'{self._nome} | {self.categoria}'
+    
+    @classmethod
+    def listar_restaurantes(cls):
+        print(f'{'Nome do restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for restaurante in cls.restaurantes:
+            print(f'{restaurante._nome.ljust(25)} | {restaurante.categoria.ljust(25)} | {restaurante.ativo}')
 
-restaurantes = [restaurante_praca, restaurante_pizza]
+    @property
+    def ativo(self):
+        return '☒' if self._ativo else '☐'
+    
+    def alternar_estado(self):
+        self._ativo = not self._ativo
 
-print(vars(restaurante_praca))
+restaurante_praca = Restaurante('praça', 'Gourmet')
+restaurante_praca.alternar_estado()
+restaurante_pizza = Restaurante('pizza express', 'Italiana')
+
+Restaurante.listar_restaurantes()
---
+++ praticas/p23.py
@@ -0,0 +1,61 @@
+class Livro:
+    def __init__(self, titulo='', autor='', paginas=0):
+        self.titulo = titulo
+        self.autor = autor
+        self.paginas = paginas
+
+    def __str__(self):
+        return f'{self.titulo} por {self.autor} - {self.paginas} páginas'
+
+    @property
+    def titulo_autor(self):
+        return f'{self.titulo} por {self.autor}'
+
+    def aumentar_paginas(self, quantidade):
+        self.paginas += quantidade
+
+class Pessoa:
+    def __init__(self, nome='', idade=0, profissao=''):
+        self.nome = nome
+        self.idade = idade
+        self.profissao = profissao
+
+    def __str__(self):
+        return f'{self.nome}, {self.idade} anos, {self.profissao}'
+
+    @property
+    def saudacao(self):
+        if self.profissao:
+            return f'Olá, sou {self.nome}! Trabalho como {self.profissao}.'
+        else:
+            return f'Olá, sou {self.nome}!'
+
+    def aniversario(self):
+        self.idade += 1
+
+# Criando 3 instâncias da classe Pessoa
+pessoa1 = Pessoa(nome='Alice', idade=25, profissao='Engenheira')
+pessoa2 = Pessoa(nome='Luiza', idade=30, profissao='Desenvolvedor')
+pessoa3 = Pessoa(nome='Jaque', idade=22)
+
+# Imprimindo informações iniciais
+print("Informações Iniciais:")
+print(pessoa1)
+print(pessoa2)
+print(pessoa3)
+print()
+
+# Utilizando o método de instância aniversario para aumentar a idade de uma pessoa
+pessoa1.aniversario()
+pessoa3.aniversario()
+
+# Imprimindo informações após aniversário
+print("Informações após aniversário:")
+print(pessoa1)
+print(pessoa3)
+print()
+
+# Utilizando o método de classe saudacao para exibir mensagens personalizadas
+print(pessoa1.saudacao)
+print(pessoa2.saudacao)
+print(pessoa3.saudacao)
