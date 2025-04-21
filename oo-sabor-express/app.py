60c426d46cee0f21de8db03a58469455e9874737
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-04-21 01:26:21 -0300

    Reorganizando repositórios e continuando com o projeto

---
+++ oo-sabor-express/app.py
@@ -0,0 +1,15 @@
+from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
+
+restaurante_praca = Restaurante('praça', 'Gourmet')
+bebida_suco = Bebida('Suco de Melância', 5.0, 'grande')
+prato_paozinho = Prato('Pãozinho', 2.00, 'O melhor pão da cidade')
+
+
+def main():
+   print(bebida_suco)
+   print(prato_paozinho)
+
+if __name__ == '__main__':
+    main()
--- praticas/p23pessoa.py
+++ praticas/p23pessoa.py
@@ -1,19 +1,3 @@
-class Livro:
-    def __init__(self, titulo='', autor='', paginas=0):
-        self.titulo = titulo
-        self.autor = autor
-        self.paginas = paginas
-
-    def __str__(self):
-        return f'{self.titulo} por {self.autor} - {self.paginas} páginas'
-
-    @property
-    def titulo_autor(self):
-        return f'{self.titulo} por {self.autor}'
-
-    def aumentar_paginas(self, quantidade):
-        self.paginas += quantidade
-
 class Pessoa:
     def __init__(self, nome='', idade=0, profissao=''):
         self.nome = nome
