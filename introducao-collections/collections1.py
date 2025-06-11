ce44a1e2a14e9f53a5719f729d2f0e0877cfb8a6
matheusmoyaoliveira <matheus.dev.moya@hotmail.com>
2025-06-11 02:58:02 -0300

    tirando exercicios de lista do versionamento

---
+++ introducao-collections/collections1.py
@@ -0,0 +1,53 @@
+idade1 = 39
+idade2 = 30
+idade3 = 27
+idade4 = 18
+
+print(idade1)
+print(idade2)
+print(idade3)
+print(idade4)
+
+idades = [39, 30, 27, 18]
+
+type(idades)
+
+len(idades)
+
+idades[0]
+
+idades
+
+print(idades[1])
+print(idades[2])
+print(idades[3])
+
+idades.append(15)
+
+idades
+
+idades[4]
+
+idades[5] #ERROR
+
+for idade in idades:
+    print(idade)
+
+idades.remove(30)
+
+idades
+
+idades.remove(30) #ValueError
+
+idades.append(15)
+
+idades
+
+idades.remove(15)
+
+idades
+
+idades.append(27)
+idades.remove(27)
+idades
+
